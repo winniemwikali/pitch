9687db41450d105ad428ec88648b854973ebbc3d
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2020-02-13 12:08:30 +0300

    removed items from app/__init__.py

--- manage.py
+++ manage.py
@@ -1,22 +1,8 @@
-from flask_script import Manager,Server,Shell
-from app import create_app,db
+from app import creat_app,db
 from app.models import User
-from flask_migrate import Migrate, MigrateCommand
-
-#Creating app instance
-app = create_app('production')
-manager = Manager(app)
-manager.add_command('server',Server)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('test')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-@manager.shell
+@manage .shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db, User = User)
+
 if __name__ == '__main__':
-    manager.run()
+    manage.run()
